a1c308dbc77b68731b23d487d8fa68e5c0527292
LarsVlyminckx <33087668+LarsVlyminckx@users.noreply.github.com>
2017-11-08 18:09:27 +0100

    Initial commit

---
+++ app/_attachments/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+angular.module('parkingApp', ['ngRoute'])
+
+	.config(function($routeProvider) {
+	    $routeProvider
+	        .when('/home', {
+	            templateUrl: 'assets/views/home.html',
+	            controller: 'homeCtrl'
+	        })
+	        .otherwise({
+                redirectTo: '/home'
+            });
+	})
+	
+	.controller('homeCtrl', function($scope, $http) {
+		
+	    	$('#searchButton').on('click', function (e) {
+
+	    		$scope.color = '';
+	    		$http.get("http://theimdbapi.org/api/find/person?name=steve+mcqueen"  + '&format=json')
+	    		  .then(function(response) {
+	    		      var tmpStr = response.data;
+	  
+	    		      $scope.color = tmpStr[0].filmography.actor;
+	    		  });
+	    	});
+    })
+    
+    ;
